7c4aad4b2b00106fb54a46f51af26b05811dd252
KatrinDiakova <diakova.ekatrina@gmail.com>
2023-07-07 18:35:08 -0300

    Stage 3 update

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -1,7 +1,11 @@
 package advisor;
 
 public class Main {
+
     public static void main(String[] args) {
+        if (args.length == 2 && args[0].equals("-access")) {
+            Config.SERVER_PATH = args[1];
+        }
         Advisor advisor = new Advisor();
         advisor.run();
     }
